638bd625ecc59ad779bffe98ad00123a2ec60d2f
svhl <145035644+svhl@users.noreply.github.com>
2024-09-03 14:46:39 +0530

    fix ssmp prog 2a

--- SSMP/day-02/prog2a.c
+++ SSMP/day-02/prog2a.c
@@ -13,6 +13,7 @@ struct block
 struct file
 {
 	char name[50];
+	int noofblocks;
 	int startblock;
 };
 
@@ -21,7 +22,7 @@ void main()
 	struct block b[50];
 	struct file f[50];
 	
-	int remblocks = 100, nb = 0, nf = 0, n;
+	int remblocks = 50, nb = 0, nf = 0, n;
 	printf("Enter no. of files:\n");
 	scanf("%d", &n);
 	
@@ -41,8 +42,10 @@ void main()
 		
 		else
 		{
-			strcpy(f[i].name, n);
-			f[i].startblock = nb;
+			remblocks -= s;
+			strcpy(f[nf].name, n);
+			f[nf].startblock = nb;
+			f[nf].noofblocks = s;
 			nf++;
 			
 			for(int j = 0; j < s - 1; j++)
@@ -56,16 +59,19 @@ void main()
 		}
 	}
 	
+	printf("File name\tFile size\tBlocks allocated\n");
+	
 	for(int i = 0; i < nf; i++)
 	{
-		int count = 0, curblock = f[i].startblock;
+		printf("%s\t\t%d\t\t", f[i].name, f[i].noofblocks);
+		int curblock = f[i].startblock;
 		
 		while(curblock != -1)
 		{
-			count++;
+			printf("%d ", curblock);
 			curblock = b[curblock].nextblock;
 		}
 		
-		printf("File:%s\t\tStart block:%d\t\tNo. of blocks:%d\n", f[i].name, f[i].startblock, count);
+		printf("\n");
 	}
 }
